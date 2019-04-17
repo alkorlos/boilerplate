cb16e5911dd77c3bb1bff9556b4a971587e7cb90
Dmitry <alkor.sx@gmail.com>
2019-04-17 12:57:35 +0400

    (js) fix: videos 1.1.4

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,6 +4,7 @@ const pug = require('./pug');
 const css = require('./css');
 const js = require('./js');
 const images = require('./images');
+const videos = require('./videos');
 const svg = require('./svg');
 const fonts = require('./fonts');
 const files = require('./files');
@@ -18,6 +19,7 @@ const watch = function(done) {
 	gulp.watch(config.src.css, gulp.series(wrapPipe(css)));
 	gulp.watch(config.src.js, gulp.series(js));
 	gulp.watch(config.src.images, gulp.series(images));
+	gulp.watch(config.src.videos, gulp.series(videos));
 	gulp.watch(config.src.svg, gulp.series(svg));
 	gulp.watch(config.src.fonts, gulp.series(fonts));
 	gulp.watch(config.src.files, gulp.series(files));
