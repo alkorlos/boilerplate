46a56bcaef8f5be4d24c6f843895d8d286b0c2c9
Dmitry <alkor.sx@gmail.com>
2019-07-29 04:46:30 +0400

    (js) new: default

---
+++ src/js/ui/button.js
@@ -0,0 +1,5 @@
+const button = function() {
+	// console.log("123");
+}
+
+export {button};
---
+++ src/js/ui/index.js
@@ -0,0 +1,7 @@
+import {button} from './button';
+
+const ui = function() {
+	button();
+}
+
+export {ui};
