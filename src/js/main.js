c80cb63eefb8501fbf0fb3338a448e531b5da780
Dmitry <alkor.sx@gmail.com>
2020-01-11 17:16:54 +0400

    (html, css, js) refactor: object-fit (IE11)

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
 import 'focus-visible'; // Полифил для :focus-visible
-import objectFitImages from 'object-fit-images'; // Полифил для object-fit
 
 import {ui} from './ui';
 
 document.addEventListener('DOMContentLoaded', function() {
-	objectFitImages(); // Полифил для object-fit
-
 	ui();
 })
