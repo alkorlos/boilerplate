dfeec4caa2d1d028761745a8fb1bfaeec809b30f
Dmitry <alkor.sx@gmail.com>
2020-01-11 17:01:20 +0400

    (html, js) refactor: svg sprite (IE11)

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,11 +3,7 @@ const gulp = require('gulp');
 const html = require('./html');
 const css = require('./css');
 // Добавляет хэш к именам css файлов и sprite.svg, генерит rev-manifest.json
-const revision = require('./revision');
-// Заменяет ссылку на sprite.svg в svg-sprite-loader.js на спрайт с хэшом в имени, грузит svg-sprite-loader в build
-const svgSpriteLoader = require('./svg-sprite-loader');
-// Удаляет старые стили и svg спрайты с хэшом в имени
-const revisionClean = require('./revision-clean');
+// const revision = require('./revision');
 // const js = require('./js'); // Не нужно после добавления webpack-dev-middleware
 const images = require('./images');
 const videos = require('./videos');
@@ -24,7 +20,7 @@ const watch = function(done) {
 	// gulp.watch(config.src.js, gulp.series(js)); // Не нужно после добавления webpack-dev-middleware
 	gulp.watch(config.src.images, gulp.series(images));
 	gulp.watch(config.src.videos, gulp.series(videos));
-	gulp.watch(config.src.svg, gulp.series(svg, revision, svgSpriteLoader, revisionClean));
+	gulp.watch(config.src.svg, gulp.series(svg)); // revision
 	gulp.watch(config.src.fonts, gulp.series(fonts));
 	gulp.watch(config.src.files, gulp.series(files));
 	done();
--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,6 @@ const css = require('./gulp/tasks/css');
 const revision = require('./gulp/tasks/revision');
 // Заменяет ссылки на стили в html шаблоне на файлы стилей с хэшом в имени
 const htmlManifest = require('./gulp/tasks/html-manifest');
-// Заменяет ссылки на svg спрайт в svg-sprite-loader.js на svg спрайт с хэшом в имени
-const svgManifest = require('./gulp/tasks/svg-manifest');
-// Заменяет ссылку на sprite.svg в svg-sprite-loader.js на спрайт с хэшом в имени, грузит svg-sprite-loader в build
-const svgSpriteLoader = require('./gulp/tasks/svg-sprite-loader');
 const js = require('./gulp/tasks/js');
 const images = require('./gulp/tasks/images');
 const videos = require('./gulp/tasks/videos');
@@ -30,8 +26,6 @@ gulp.task('start',
 		videos,
 		svg,
 		revision,
-		svgSpriteLoader,
-		svgManifest,
 		fonts,
 		files,
 		watch,
@@ -50,8 +44,6 @@ gulp.task('build',
 		svg,
 		revision,
 		htmlManifest,
-		svgSpriteLoader,
-		svgManifest,
 		fonts,
 		files
 	)
