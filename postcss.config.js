6179532276bedcf939fc8cc5b1eb0088ab5cef70
Dmitry <alkor.sx@gmail.com>
2020-01-11 17:18:42 +0400

    (js) refactor: postcss-flexbugs-fixes (IE11)

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
 	plugins: {
 		'postcss-import-ext-glob': {},
 		'postcss-import': {},
-		'postcss-flexbugs-fixes': {},
 		'postcss-preset-env': {
 			stage: 1,
 			autoprefixer: true
