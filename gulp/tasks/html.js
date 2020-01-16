d8f1b9540c47d5e6b477fdd1704b4a105f488055
Dmitry <alkor.sx@gmail.com>
2020-01-16 23:54:48 +0400

    (js, json) new: pretty-html

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const nunjucks = require('gulp-nunjucks');
+const prettyHtml = require('gulp-pretty-html');
 
 const config = require('../config');
 
@@ -13,6 +14,13 @@ const html = function(done) {
 		.pipe(
 			nunjucks.compile()
 		)
+		.pipe(prettyHtml(
+			{
+				indent_size: 4,
+				indent_char: '	',
+				max_preserve_newlines: 1
+			}
+		))
 		.pipe(gulp.dest(config.build.html))
 		.on('end', done);
 }
