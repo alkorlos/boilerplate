b004f0236343d3ac2cb726f9972e5a3b67e96f36
Dmitry <alkor.sx@gmail.com>
2020-09-17 15:14:54 +0400

    (js, json) new: gulp-changed images

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,10 +1,13 @@
 const gulp = require('gulp');
 
+const changed = require('gulp-changed');
+
 const config = require('../config');
 
 // Images
 const images = function () {
 	return gulp.src(config.src.images)
+		.pipe(changed(config.build.images))
 		.pipe(gulp.dest(config.build.images));
 };
 
