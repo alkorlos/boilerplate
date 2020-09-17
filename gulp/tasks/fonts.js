c40d2c79c21b82f0c770946c1f786975bed91521
Dmitry <alkor.sx@gmail.com>
2020-09-17 15:17:50 +0400

    (js) new: gulp-changed fonts

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,10 +1,13 @@
 const gulp = require('gulp');
 
+const changed = require('gulp-changed');
+
 const config = require('../config');
 
 // Fonts
 const fonts = function () {
 	return gulp.src(config.src.fonts)
+		.pipe(changed(config.build.fonts))
 		.pipe(gulp.dest(config.build.fonts));
 };
 
