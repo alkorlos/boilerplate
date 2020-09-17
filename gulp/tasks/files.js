4a836fd4c9b4e5dad77374b0b185ae18ae221304
Dmitry <alkor.sx@gmail.com>
2020-09-17 15:31:18 +0400

    (js) new: gulp-changed files

--- gulp/tasks/files.js
+++ gulp/tasks/files.js
@@ -1,10 +1,13 @@
 const gulp = require('gulp');
 
+const changed = require('gulp-changed');
+
 const config = require('../config');
 
 // Files
 const files = function () {
 	return gulp.src(config.src.files)
+		.pipe(changed(config.build.files))
 		.pipe(gulp.dest(config.build.files));
 };
 
