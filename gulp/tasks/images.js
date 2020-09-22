ce9c3e8053bb2abe04b0738ef0e5396df3e5f5b4
Dmitry <alkor.sx@gmail.com>
2020-09-22 19:38:59 +0400

    (js) refactor: bundler images

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,15 +1,34 @@
 const gulp = require('gulp');
 
 const changed = require('gulp-changed');
+const imagemin = require('gulp-imagemin');
 
 const config = require('../config');
 
 // Images
 const images = function () {
 	return gulp.src(config.src.images)
-		.pipe(changed(config.build.images, {
-			hasChanged: changed.compareContents
-		}))
+		.pipe(changed(config.build.images))
+		.pipe(imagemin([
+			imagemin.mozjpeg({
+				quality: 80
+			}),
+			imagemin.optipng({
+				optimizationLevel: 3
+			}),
+			imagemin.svgo({
+				plugins: [
+					{
+						cleanupIDs: {
+							minify: false
+						}
+					},
+					{
+						removeViewBox: false
+					}
+				]
+			})
+		]))
 		.pipe(gulp.dest(config.build.images));
 };
 
