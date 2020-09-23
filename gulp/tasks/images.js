2b0d9d12b9b6d6d39510c16a4c133fabfec696a3
Dmitry <alkor.sx@gmail.com>
2020-09-23 18:34:35 +0400

    (js, json) new: bundler mode production and development

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -4,36 +4,42 @@ const changed = require('gulp-changed');
 const imagemin = require('gulp-imagemin');
 // const imageminwebp = require('imagemin-webp');
 
-const config = require('../config');
+const gulpif = require('gulp-if');
+const mode = process.env.NODE_ENV;
 
+const config = require('../config');
 
 // Images
 const images = function () {
 	return gulp.src(config.src.images)
-		.pipe(changed(config.build.images))
-		.pipe(imagemin([
-			// imageminwebp({
-			// 	quality: 80
-			// }),
-			imagemin.mozjpeg({
-				quality: 80
-			}),
-			imagemin.optipng({
-				optimizationLevel: 3
-			}),
-			imagemin.svgo({
-				plugins: [
-					{
-						cleanupIDs: {
-							minify: false
+		.pipe(changed(config.build.images, {
+			hasChanged: changed.compareContents
+		}))
+		.pipe(gulpif(mode === 'production',
+			imagemin([
+				// imageminwebp({
+				// 	quality: 80
+				// }),
+				imagemin.mozjpeg({
+					quality: 80
+				}),
+				imagemin.optipng({
+					optimizationLevel: 3
+				}),
+				imagemin.svgo({
+					plugins: [
+						{
+							cleanupIDs: {
+								minify: false
+							}
+						},
+						{
+							removeViewBox: false
 						}
-					},
-					{
-						removeViewBox: false
-					}
-				]
-			})
-		]))
+					]
+				})
+			])
+		))
 		.pipe(gulp.dest(config.build.images));
 };
 
