2be38435bd28cfe90680b50b3b96d8959e513dc0
Dmitry Statsenko <alkor.sx@gmail.com>
2023-02-05 13:43:05 +0400

    update del

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -1,10 +1,10 @@
-import del from 'del';
+import { deleteSync } from 'del';
 
 import config from '../config.js';
 
 // Clean
 export const clean = function () {
-	return del(config.dist.root + '/**/*');
+	return deleteSync([config.dist.root + '/*/']);
 };
 
 export default clean;
