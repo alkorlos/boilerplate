5896d150ed62a82a25ccaa57ad112e6e78a800be
Dmitry Statsenko <alkor.sx@gmail.com>
2024-03-05 12:39:14 +0300

    fix broken css minification

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,10 @@ module.exports = {
 		'postcss-import-ext-glob': {},
 		'postcss-import': {},
 		'postcss-preset-env': {
-			stage: 2
+			stage: 2,
+			features: {
+				'media-query-ranges': true // fix broken css minification
+			}
 		}
 	}
 };
